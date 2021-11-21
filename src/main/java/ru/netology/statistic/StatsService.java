7286232808f9e28a5d8fdd35d24fd402fb30f03f
Ufimskii <alexander1383@list.ru>
2021-11-21 17:51:34 +0500

    add_executions

--- src/main/java/ru/netology/statistic/StatsService.java
+++ src/main/java/ru/netology/statistic/StatsService.java
@@ -2,70 +2,19 @@ package ru.netology.statistic;
 
 public class StatsService {
 
-    //-----------------Первый метод (Сумма всех продаж)----------------------//
-    public long calculateSum(long[] sales) {
-        long sum = 0;
-        for (long sale : sales) {
-            sum += sale;
-        }
-        return sum;
-    }
-
-    //-----------------Второй метод (Средняя сумма продаж)----------------------//
-    public long calculateMidSum(long[] sales) {
-        return calculateSum(sales) / sales.length;
-    }
-
-    // -----------------Третий метод(Номер месяца с максимальной суммой продаж)----------------------//
-    public int monthMaxSales(long[] sales) {
-        int maxMonth = 0;
-        int month = 0;
-        for (long sale : sales) {
-            if (sale >= sales[maxMonth]) {
-                maxMonth = month;
-            }
-            month = month + 1;
-        }
-        return maxMonth + 1;
-    }
-
-    //---------------Четвертый метод(Номер месяца с минимальной суммой продаж)---------------------//
-    public int monthMinSales(long[] sales) {
-        int minMonth = 0;
-        int month = 0;
-        for (long sale : sales) {
-            if (sale <= sales[minMonth]) {
-                minMonth = month;
-            }
-            month = month + 1;
-        }
-        return minMonth + 1;
-    }
-
-    // -----------------Пятый метод(Кол-во месяцев, в которых продажи были ниже среднего)------------//
-    public int countMonthLessMid(long[] sales) {
-        int lessMid = 0;
-        //long midSum = calculateSum(sales) / sales.length;
-        long midSum = calculateMidSum(sales);
-        for (long sale : sales) {
-            if (sale < midSum) {
-                lessMid = lessMid + 1;
-            }
-        }
-        return lessMid;
-    }
-
-    // -----------------Шестой метод (Кол-во месяцев, в которых продажи были выше среднего)------------//
-    public int countMonthMoreMid(long[] sales) {
-        int moreMid = 0;
-        //long midSum = calculateSum(sales) / sales.length;
-        long midSum = calculateMidSum(sales);
-        for (long sale : sales) {
-            if (sale > midSum) {
-                moreMid = moreMid + 1;
-            }
-        }
-        return moreMid;
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        return current_max;
     }
 }
 
--- src/test/java/ru/netology/statistic/StatsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatsServiceTest.java
@@ -6,64 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
 
-    // =======================  Тест №1  ==============================//
     @Test
-    void shouldCalculateSum() {
+    void findMax() {
         StatsService service = new StatsService();
-        long[] yearSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 180;
-        long actual = service.calculateSum(yearSales);
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
         assertEquals(expected, actual);
     }
-
-    // ======================  Тест №2  ================================//
-    @Test
-    void shouldCalculateMiddleSum() {
-        StatsService service = new StatsService();
-        long[] yearSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 15;
-        long actual = service.calculateMidSum(yearSales);
-        assertEquals(expected, actual);
-    }
-
-    // ======================  Тест №3  ================================//
-    @Test
-    void shouldMonthMaxSales() {
-        StatsService service = new StatsService();
-        long[] yearSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 8;
-        long actual = service.monthMaxSales(yearSales);
-        assertEquals(expected, actual);
-    }
-
-    // ======================  Тест №4  ================================//
-    @Test
-    void shouldMonthMinSales() {
-        StatsService service = new StatsService();
-        long[] yearSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 9;
-        long actual = service.monthMinSales(yearSales);
-        assertEquals(expected, actual);
-    }
-
-    // ======================  Тест №5  ================================//
-    @Test
-    void shouldMonthLessMid() {
-        StatsService service = new StatsService();
-        long[] yearSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 5;
-        long actual = service.countMonthLessMid(yearSales);
-        assertEquals(expected, actual);
-    }
-
-    // ======================  Тест №6  ================================//
-    @Test
-    void shouldMonthMoreMid() {
-        StatsService service = new StatsService();
-        long[] yearSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 5;
-        long actual = service.countMonthMoreMid(yearSales);
-        assertEquals(expected, actual);
-    }
-
 }
